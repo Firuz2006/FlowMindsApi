bda1930cc9d62f7cefe61a8500dfff832b665b01
firuz2006 <firuzisoboev@gmail.com>
2024-05-14 16:45:55 +0500

    mongo init

---
+++ FlowMindsApi/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FlowMindsApi.Models;
+
+public class Category
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Parent_id { get; set; }
+    public string Organization_id { get; set; }
+}
